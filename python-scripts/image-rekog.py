211a0182e004f051fcabecbb4792f4853c0d6193
silpapriyadarshini <silpa.priyadarshini@cloudreach.com>
2022-07-29 14:01:29 +0530

    added more code in python script

--- python-scripts/image-rekog.py
+++ python-scripts/image-rekog.py
@@ -15,24 +15,39 @@ def lambda_handler(event, context):
         bucket_name = record["s3"]["bucket"]["name"]
         image_obj = record["s3"]["object"]["key"]
 
-    amount_of_faces = 0
+    number_of_faces = 0
     male_face = 0
     female_face = 0
+    eye_glasses=0
+    sun_glasses=0
+    Mustache_face=0
 
-    print(amount_of_faces)
+    print(number_of_faces)
 
     response = client.detect_faces(Image={'S3Object':{'Bucket':bucket_name,'Name':image_obj}},Attributes=['ALL'])
 
     faces = response["FaceDetails"]
 
-    amount_of_faces = len(faces)
-    print(amount_of_faces)
+    number_of_faces = len(faces)
+    print(number_of_faces)
 
     for face in faces:
         if face["Gender"]["Value"] == "Male":
             male_face += 1
         elif face["Gender"]["Value"] == "Female":
             female_face += 1
+    
+    for face in faces:
+        if face["Eyeglasses"]["Value"] == "true":
+            eye_glasses +=1
+
+    for face in faces:
+        if face["Mustache"]["Value"] == "true":
+            Mustache_face +=1
+
+    for face in faces:
+        if face["Sunglasses"]["Value"] == "true":
+            sun_glasses +=1
 
     table = dynamodb_resource.Table(metadata_table)
 
@@ -40,7 +55,10 @@ def lambda_handler(event, context):
         "filename": image_obj,
         "amount_of_faces": amount_of_faces,
         "male_faces": male_face,
-        "female_faces": female_face
+        "female_faces": female_face,
+        "eye_glasses": eye_glasses,
+        "mustache_faces": Mustache_face,
+        "sun_glasses": sun_glasses
     }
 
     table.put_item(Item=metadata)
